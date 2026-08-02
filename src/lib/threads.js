4b78c282b50c02cde205f355da501933281ae60b
anhtuan0806 <truongtuananh200806@gmail.com>
2026-08-03 00:05:29 +0700

    revert: restore original threads worker path string

--- src/lib/threads.js
+++ src/lib/threads.js
@@ -7,10 +7,6 @@ const {
 } = require('threads');
 const { cpus } = require('node:os');
 
-function getWorkerPath(name) {
-	return path.relative(process.cwd(), path.join(__dirname, 'workers', `${name}.js`));
-}
-
 /**
  * Create a single-use thread pool
  * @param {number} num fraction of available CPUs to use (ceil'd), or absolute number
@@ -36,7 +32,7 @@ async function quickPool(num, name, fun, options) {
  */
 function reusablePool(num, name, options) {
 	const size = num < 1 ? Math.ceil(num * (parseInt(process.env.CPU_LIMIT) || cpus().length)) : num;
-	const pool = Pool(() => spawn(new Worker(getWorkerPath(name))), {
+	const pool = Pool(() => spawn(new Worker(`./workers/${name}.js`)), {
 		...options,
 		size,
 	});
@@ -50,7 +46,7 @@ function reusablePool(num, name, options) {
  * @returns {Promise<any}
  */
 async function quick(name, fun) {
-	const thread = await spawn(new Worker(getWorkerPath(name)));
+	const thread = await spawn(new Worker(`./workers/${name}.js`));
 	try {
 		// ! this await is extremely important
 		return await fun(thread);
@@ -65,7 +61,7 @@ async function quick(name, fun) {
  * @returns {Promise<{terminate: function}>}
  */
 async function reusable(name) {
-	const thread = await spawn(new Worker(getWorkerPath(name)));
+	const thread = await spawn(new Worker(`./workers/${name}.js`));
 	thread.terminate = () => Thread.terminate(thread);
 	return thread;
 };
